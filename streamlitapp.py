80cac7c0a6147eb8e40c318fb6289dd55979d864
Abubaker Shangab <shangab@gmail.com>
2023-03-07 21:25:17 -0500

    ok

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,5 +1,38 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import openai
+from streamlit_chat import message
 
-st.title('Uber pickups in NYC')
+from config import OPENAI_API_KEY
+openai.api_key = OPENAI_API_KEY
+
+
+def get_completions(prompt):
+    completions = openai.Completion.create(
+        engine='text-davinci-003', max_tokens=100, prompt=prompt)
+    return completions.choices
+
+
+st.title('Playing with NLP and OpenAI GPT-3.5 using the amazing Streamlit')
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = []
+
+if 'prompts' not in st.session_state:
+    st.session_state['prompts'] = []
+
+def get_prompt():
+    prompt= st.text_input("You:", "Hello how are you?",key="input")
+    return prompt
+
+prompt = get_prompt()
+
+if prompt:
+    results = get_completions(prompt)
+    st.session_state.prompts.append(prompt)
+    st.session_state.generated.append(results[0].text)
+
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state['prompts'][i], is_user=True, key=str(i) + '_user')
