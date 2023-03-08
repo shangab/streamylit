ab78d5c4f48c5a87632c586e27a9401b4d58f8a3
Abubaker Shangab <shangab@gmail.com>
2023-03-07 21:58:26 -0500

    ok

--- streamlitapp.py
+++ streamlitapp.py
@@ -2,7 +2,7 @@ import streamlit as st
 import openai
 from streamlit_chat import message
 
-openai.api_key = OPENAI_API_KEY
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 
 def get_completions(prompt):
