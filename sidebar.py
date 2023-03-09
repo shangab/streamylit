b8b78bc684b6b10eca7ef7021cd190777aba65c0
Abubaker Shangab <shangab@gmail.com>
2023-03-08 23:12:17 -0500

    ok

--- sidebar.py
+++ sidebar.py
@@ -1,15 +1,26 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+
 
 def show():
     with st.sidebar:
         st.image("./assets/img/abu2.jpeg", width=120)
         st.subheader('By: Abubaker Shangab')
-        st.session_state.nlp= st.radio("Natural Language Processing",("Huggingface Transformrs","NLP Operations", "Openai API Models"))
-        st.session_state.ml=st.radio("Machine Learning",("Tensorflow Keras","Pytorch"))
-        st.session_state.bi=st.radio("Business Intelligence",("Pandas Dashboard","BI Basics"))
-        
-        if st.session_state.nlp=='Openai API Models':
-            st.session_state.nlp= st.radio("Openai API Models",("GPT-3 Completions","GPT-3 Classification", "GPT-3 Summarization"))
-
-        if st.session_state.ml=='Tensorflow Keras':
-            st.session_state.ml=st.radio("Keras Classifications",("Example 1","Example 2"))
+        st.session_state.page = option_menu("App Menu",
+                                            options=["Huggingface Transformrs",
+                                                     "GPT-3 Completions",
+                                                     "GPT-3 Classification",
+                                                     "GPT-3 Summarization",
+                                                     "GPT-3 Image Generation",
+                                                     "Keras Example 1",
+                                                     "Keras Example 2",
+                                                     "Pandas Dataframe",
+                                                     ],
+                                            icons=[
+                                                "emoji-smile-fill",
+                                                "chat-dots",
+                                                "layout-split",
+                                                "arrows-collapse",
+                                                "chat-dots",
+                                                "chat-dots",
+                                            ], menu_icon="cast")
--- streamlitapp.py
+++ streamlitapp.py
@@ -4,29 +4,38 @@ import gpt3
 import ml
 
 with open('assets/css/styles.css') as css:
-    st.markdown(f'<style>{css.read()}</style>',unsafe_allow_html=True)
+    st.markdown(f'<style>{css.read()}</style>', unsafe_allow_html=True)
+subheaders = {
+    "GPT-3 Completions": "GPT-3 Completions text-davinci-003 Engine",
+    "GPT-3 Classification": "a",
+    "GPT-3 Summarization": "b",
+    "GPT-3 Image Generation": "Image Generation Model",
+    "Keras Example 1": "d",
+    "Keras Example 2": "e",
+    "Pandas Dataframe": "f",
+}
+if not ("page" in st.session_state):
+    st.title("Application for AI")
+    st.subheader('Different Applications using AI tools and libraries')
 
-st.title('NLP, GPT-3 and Machine Learning')
-st.subheader('Different Applications using AI tools and libraries')
 
-st.session_state.page=''
+def showheaders():
+    st.title(st.session_state.page)
+    st.subheader(subheaders[st.session_state.page])
+
 
 sidebar.show()
 
 
-if st.session_state.nlp=="GPT-3 Completions":
+if st.session_state.page == "GPT-3 Completions":
+    showheaders()
     gpt3.completions()
-
-if st.session_state.nlp=="GPT-3 Classification":
-    st.session_state.ml=''
+if st.session_state.page == "GPT-3 Classification":
+    showheaders()
     gpt3.classification()
-if st.session_state.nlp=="GPT-3 Summarization":
-    st.session_state.ml=''
+if st.session_state.page == "GPT-3 Summarization":
+    showheaders()
     gpt3.summarization()
-
-if st.session_state.ml=='Example 1':
-    st.session_state.nlp=''
-    ml.kerasExample1()
-if st.session_state.ml=='Example 2':
-    st.session_state.nlp=''
-    ml.kerasExample2()
+if st.session_state.page == "GPT-3 Image Generation":
+    showheaders()
+    gpt3.image()
